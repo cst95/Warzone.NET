e1e649c6e693fecea17d61385bbec1da44d93ac9
Charlie Stokes <cstokes873@gmail.com>
2021-03-14 17:21:51 +0000

    Move validation to its own private method and add some XML docs on the WarzoneClient methods.

--- Warzone/IWarzoneClient.cs
+++ Warzone/IWarzoneClient.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading;
 using System.Threading.Tasks;
+using Warzone.Exceptions;
 using Warzone.Models;
 using Warzone.Models.CodApi;
 
@@ -7,9 +8,26 @@ namespace Warzone
 {
     public interface IWarzoneClient
     {
+        /// <summary>
+        ///  Fetch summarized data for the user's last twenty Warzone matches.
+        ///  Only includes standard BR matches (Solo, Duos, Trios, Quads).
+        /// </summary>
+        /// <param name="playerName"></param>
+        /// <param name="platform"></param>
+        /// <param name="cancellationToken"></param>
+        /// <exception cref="NotLoggedInException">You're login has expired and you should reauthenticate with the LoginAsync method.</exception>
+        /// <exception cref="WarzoneException">A generic catch all exception indicating something went wrong.</exception>
+        /// <returns></returns>
         Task<WarzoneResponse<Summaries>> GetLastTwentyWarzoneMatchesAsync(string playerName, string platform,
             CancellationToken? cancellationToken);
 
+        /// <summary>
+        /// Log into the Call of Duty API with a valid account before attempting to access other endpoints.
+        /// Login valid for approximately two hours, after which point you will be required to re-login with this method.
+        /// </summary>
+        /// <param name="email"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
         Task<bool> LoginAsync(string email, string password);
     }
 }
